c0df03ad69e55951946ba00384e51d729c25fa2d
khaimtp1417 <95134713+khaimtp1417@users.noreply.github.com>
2021-12-29 23:51:59 +0700

    Add files via upload

--- cau1.c
+++ cau1.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
-int check(int n,int i)
+int NotMain(int n)
 {
-    int count=0;
-    if(i==10)
+    int sum=0;
+    for(int i=1;i<=n;i++)
     {
-        return count;
+        sum=sum + i/2;
     }
-    else
-    {
-        if(n%i==0)
-        {
-            return count=count+1;
-        }
-    }
-    return count+check(n,i+1);
-}
-int checkin(int n)
-{
-    return check(n,2);
+    return sum;
 }
 int main()
 {
     int n;
-    printf("nhap mot so:");
+    printf("nhap n:");
     scanf("%d",&n);
-    int x=checkin(n);
-    if(x==0)
-    {
-        printf("day la so nguyen to");
-    }
-    else
-    {
-        printf("day khong phai so nguyen to");
-    }
-    return 0;
+
+    printf("%d",NotMain(n));
 }
